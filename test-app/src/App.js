1ecba2c15b3e7c19212d1b88c24ab9abbd71a489
murphytkh <murphytkh@email.com>
2021-03-04 20:15:29 +0800

    BITCH

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React BITCH
         </a>
       </header>
     </div>
